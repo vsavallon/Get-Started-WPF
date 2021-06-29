b1e62e528efee547c42f57a6a88eb9cb20e23651
vsavallon <vishal@techqware.com>
2021-06-29 13:47:17 +0530

    Pocess Monitor updated with including priorities...

--- ProcessMonitor/ProcessMonitor/Monitor.xaml.cs
+++ ProcessMonitor/ProcessMonitor/Monitor.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -21,6 +23,46 @@ namespace ProcessMonitor
         public Monitor()
         {
             InitializeComponent();
+        }
+
+        private void sortingCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetNewSortingOrder();
+        }
+
+        private void SetNewSortingOrder()
+        {
+            string sortTypeName = ((ComboBoxItem)sortingCombo.SelectedItem).Content.ToString();
+            SortDescription sortDesp = new SortDescription(sortTypeName, ListSortDirection.Ascending);
+            CollectionViewSource source = FindResource("processesView") as CollectionViewSource;
+            source.SortDescriptions.Clear();
+            source.SortDescriptions.Add(sortDesp);
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            Process p = e.Item as Process;
+            int mode = (priorityCombo != null) ? priorityCombo.SelectedIndex : 0;
+            switch (mode)
+            {
+                case 1:
+                    e.Accepted = (p.BasePriority < 8); 
+                    break;
+                case 2:
+                    e.Accepted = (p.BasePriority > 12); 
+                    break;
+                case 3:
+                    e.Accepted = (p.BasePriority >= 8 && p.BasePriority <= 12);
+                    break;
+                default:
+                    e.Accepted = true;
+                    break;
+            }
+        }
+
+        private void priorityCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SetNewSortingOrder();
         }
     }
 }
