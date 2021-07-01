9db7128c3a843afe7dc5d5cfda8683975d5f6310
vsavallon <vishal@techqware.com>
2021-07-01 13:34:57 +0530

    login & register in WPF

--- SamplesInWPF/SamplesInWPF/Login.xaml.cs
+++ SamplesInWPF/SamplesInWPF/Login.xaml.cs
@@ -88,5 +88,106 @@ namespace SamplesInWPF
         {
             Application.Current.Shutdown();
         }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckValidations())
+            {
+                return;
+            }
+            if (IsUserIdAvail(txtUsername.Text))
+            {
+                SaveUser(txtUsername.Text, txtPassword.Password);
+            }
+            else
+            {
+                MessageBox.Show("User Already Registered!! PLease try another username or click on login...", "Username status", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private bool CheckValidations()
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Enter Username!");
+                return false;
+            }
+            if(txtPassword.Password == "")
+            {
+                MessageBox.Show("Enter Password!");
+                return false;
+            }
+            if(txtPassword.Password.Length <= 6)
+            {
+                MessageBox.Show("Must enter more than 6 characters in password box!");
+                return false;
+            }
+            return true;
+        }
+
+        private void SaveUser(string userid, string password)
+        {
+            try
+            {
+                conn.Open();
+                string query = "sp_insertUser";
+                cmd = new SqlCommand(query, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UserId", userid);
+                cmd.Parameters.AddWithValue("@Passwd", password);
+                int result = cmd.ExecuteNonQuery();
+                if(result > 0)
+                {
+                    MessageBox.Show($"User Registered Successfully!!", "Registration", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Server Down, try after sometime!!", "Registration", MessageBoxButton.OK, MessageBoxImage.Hand);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Server Not Responding", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private bool IsUserIdAvail(string userid)
+        {
+            try
+            {
+                conn.Open();
+                string sqlQuery = $"Select Status from Login_Master where Role='user' and userid='{userid}'";
+                cmd = new SqlCommand(sqlQuery, conn);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    if (Convert.ToBoolean(dr["Status"]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Server Not Responding", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return true;
+        }
     }
 }
